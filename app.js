00f09ecdb5f176a8a7572a7daa51786a3ee6e7fe
dpiyumal2319 <132676592+dpiyumal2319@users.noreply.github.com>
2024-08-30 03:57:06 +0530

    chore: Update app.js and package.json

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-//jshint esversion:6
+import ejs from "ejs";
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
