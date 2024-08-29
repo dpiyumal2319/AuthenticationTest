33789456c4f59a29212ed5ebc8da71cfac578770
dpiyumal2319 <132676592+dpiyumal2319@users.noreply.github.com>
2024-08-30 04:44:38 +0530

    chore: Add static file serving and user registration functionality

--- app.js
+++ app.js
@@ -7,6 +7,57 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String,
+});
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.route("/register")
+    .get((req, res) => {
+        res.render("register");
+    })
+    .post((req, res) => {
+        const newUser = new User({
+            email: req.body.username,
+            password: req.body.password,
+        });
+
+        User.findOne({
+            email: req.body.username
+        }).then((user) => {
+            if (user) {
+                res.redirect("/user-exists");
+            } else {
+                newUser.save().then(() => {
+                    res.render("secrets");
+                }
+                ).catch((err) => {
+                    console.log(err);
+                });
+            }
+        });
+
+        
+    });
+
+
+app.get("/user-exists", (req, res) => {
+    res.render("userExists");
+});
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
